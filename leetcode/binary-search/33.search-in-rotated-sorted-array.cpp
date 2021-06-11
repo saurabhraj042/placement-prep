404ff8787e0921a1bd55db82f3b17fc4a1e625c2
saurabhraj042 <saurabhraj042@gmail.com>
2021-06-11 13:58:09 +0530

    add leetcode soln 33 Optimal*

--- leetcode/binary-search/33.search-in-rotated-sorted-array.cpp
+++ leetcode/binary-search/33.search-in-rotated-sorted-array.cpp
@@ -4,25 +4,22 @@
 class Solution {
 public:
     int search(vector<int>& A, int target) {
-        int n = A.size();
-        int lo=0,hi=n-1;
-       
-        while(lo<hi){
-            int mid=(lo+hi)/2;
-            if(A[mid]>A[hi]) lo=mid+1;
-            else hi=mid;
-        }
-        int rot=lo;
-        lo=0;hi=n-1;
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            int realmid=(mid+rot)%n;
-            if(A[realmid]==target)return realmid;
-            if(A[realmid]<target)lo=mid+1;
-            else hi=mid-1;
+        int lo = 0, hi = nums.size();
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            
+            double num = (nums[mid] < nums[0]) == (target < nums[0])
+                    ? nums[mid]
+                    : target < nums[0] ? -INFINITY : INFINITY;
+                    
+            if (num < target)
+                lo = mid + 1;
+            else if (num > target)
+                hi = mid;
+            else
+                return mid;
         }
         return -1;
-        
     }
 };
 
