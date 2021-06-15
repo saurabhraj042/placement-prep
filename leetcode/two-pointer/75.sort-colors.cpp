07915183a860af8dd2c0c65d74b167af53adb427
saurabhraj042 <saurabhraj042@gmail.com>
2021-06-15 07:56:53 +0530

    add leetcode soln 75

---
+++ leetcode/two-pointer/75.sort-colors.cpp
@@ -0,0 +1,24 @@
+// saurabhraj042
+//https://leetcode.com/problems/sort-colors/
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo=0,mid=0,hi=(nums.size())-1;
+        
+        while(mid<=hi){
+            int c = nums[mid];
+            
+            if( c==0 ){
+                swap( nums[mid],nums[lo] );
+                mid++;
+                lo++;
+            }else if( c==1 ){
+                mid++;
+            }else{
+                swap( nums[mid],nums[hi] );
+                hi--;
+            }
+        }
+    }
+};
+
