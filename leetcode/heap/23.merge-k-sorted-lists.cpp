d80d1a52ab93e3dc83df610f9fe577db6c2b310f
saurabhraj042 <saurabhraj042@gmail.com>
2021-06-27 12:27:37 +0530

    add leetcode soln 23

---
+++ leetcode/heap/23.merge-k-sorted-lists.cpp
@@ -0,0 +1,31 @@
+// saurabhraj042
+//https://leetcode.com/problems/merge-k-sorted-lists/
+class Solution {
+public:
+   struct compare {
+    bool operator()(const ListNode* l, const ListNode* r) {
+        return l->val > r->val;
+    }
+    };
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        priority_queue<ListNode *, vector<ListNode *>, compare> q;
+        
+        for(auto l : lists) {
+            if(l)  q.push(l);
+        }
+        if(q.empty())  return NULL;
+
+        ListNode* result = q.top();
+        q.pop();
+        if(result->next) q.push(result->next);
+        ListNode* tail = result;            
+        
+        while(!q.empty()) {
+            tail->next = q.top();
+            q.pop();
+            tail = tail->next;
+            if(tail->next) q.push(tail->next);
+        }
+        return result;
+    }
+};
