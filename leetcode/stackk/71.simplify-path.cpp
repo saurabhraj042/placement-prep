cd2828e893608123fee15c7e592bb4622816e61f
saurabhraj042 <saurabhraj042@gmail.com>
2021-06-29 18:08:23 +0530

    add leetcode soln 71*

--- leetcode/stackk/71.simplify-path.cpp
+++ leetcode/stackk/71.simplify-path.cpp
@@ -2,46 +2,30 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        int n=path.length();
-        stack<string>s;
-                
-        if(path[n-1]!='/') // so that initially path always ends with '/'
-            path+="/", ++n;
+        stack<string> s;
+        int n = path.length();
+        
+        for(int i=0;i<n;i++){
+            string c="";
             
-        int i=1; // since path always starts from '/'
-        string ans="";
-        string temp="";
-        while(i<n){
+            while(i<n && path[i]!='/') c+=path[i++];
             
-            if(path[i]=='/'){ // check only if we encounter '/'
-                
-                if(temp=="" || temp=="."){
-                    // ignore
-                }
-                else if(temp==".."){
-                    if(!s.empty()) s.pop(); // pop the top element from stack if exists
-                }
-                else{
-                    s.push(temp); //push the directory or file name to stack
-                }
-                
-                temp=""; // reset temp
+            if(c=="" || c==".") continue;
+            else if(c==".."){
+                if(!s.empty()) s.pop();
+            }else{
+                s.push(c);
             }
-            else{
-                temp.push_back(path[i]); // else append to temp
-            }
-            
-            ++i; // increment index
         }
         
-        while(!s.empty()){ // add all the stack elements
-            ans="/"+s.top()+ans;
+        string ans ="";
+        
+        while(!s.empty()){
+            ans = '/' + s.top() + ans;
             s.pop();
         }
         
-        if(ans.length()==0) // if no directory or file is present
-            ans="/"; // minimum root directory must be present in ans
-        
+        if(ans=="") return "/";
         return ans;
     }
 };
