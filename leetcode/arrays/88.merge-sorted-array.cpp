6ed4c1b347125d3ae93126f4f26c6b43d64ed8d4
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-24 11:25:57 +0530

    add optimal soln leetcode 88

--- leetcode/arrays/88.merge-sorted-array.cpp
+++ leetcode/arrays/88.merge-sorted-array.cpp
@@ -2,7 +2,11 @@
 class Solution {
 public:
     void merge(vector<int>& A, int m, vector<int>& B, int n) {
-        // Simply has to design merge function
+       soln_2(A,m,B,n);
+    }
+    
+    void soln_1(vector<int>& A, int m, vector<int>& B, int n){
+         // Simply has to design merge function
         
         int i=0,j=0;
         vector<int> aux;
@@ -20,6 +24,25 @@ public:
         
         for(i=0;i<aux.size();i++){
             A[i] = aux[i];
+        }
+    }
+    
+    // In place merge operation
+    void soln_2(vector<int>& A, int m, vector<int>& B, int n){
+        int i = m-1;
+        int j = n-1;
+        int idx = m+n-1;
+        
+        while(i >= 0 && j >= 0){
+            if(A[i] > B[j]){
+                A[idx--] = A[i--];
+            }else{
+                A[idx--] = B[j--];
+            }
+        }
+        
+        while(j >= 0){
+            A[idx--] = B[j--];
         }
     }
 };
