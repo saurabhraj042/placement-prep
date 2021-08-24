09c2c9fc42ce941a9b8ab074d7550a3b528f2cb0
saurabhraj042 <saurabhraj042@gmail.com>
2021-08-24 18:41:52 +0530

    add comments leetcode soln 1509

--- leetcode/brain-teasers/1509.minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ leetcode/brain-teasers/1509.minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -49,6 +49,44 @@ public:
     }
     
     /*
-        
+ 
+
+Intuition
+If we can do 0 move, return max(A) - min(A)
+If we can do 1 move, return min(the second max(A) - min(A), the max(A) - second min(A))
+and so on.
+
+
+Explanation
+We have 4 plans:
+
+kill 3 biggest elements
+kill 2 biggest elements + 1 smallest elements
+kill 1 biggest elements + 2 smallest elements
+kill 3 smallest elements
+
+Example from @himanshusingh11:
+
+A = [1,5,6,13,14,15,16,17]
+n = 8
+
+Case 1: kill 3 biggest elements
+
+All three biggest elements can be replaced with 14
+[1,5,6,13,14,15,16,17] -> [1,5,6,13,14,14,14,14] == can be written as A[n-4] - A[0] == (14-1 = 13)
+
+Case 2: kill 2 biggest elements + 1 smallest elements
+
+[1,5,6,13,14,15,16,17] -> [5,5,6,13,14,15,15,15] == can be written as A[n-3] - A[1] == (15-5 = 10)
+
+Case 3: kill 1 biggest elements + 2 smallest elements
+
+[1,5,6,13,14,15,16,17] -> [6,6,6,13,14,15,16,16] == can be written as A[n-2] - A[2] == (16-6 = 10)
+
+Case 4: kill 3 smallest elements
+
+[1,5,6,13,14,15,16,17] -> [13,13,13,13,14,15,16,17] == can be written as A[n-1] - A[3] == (17-13 = 4)
+
+Answer is minimum of all these cases!
     */
 };
