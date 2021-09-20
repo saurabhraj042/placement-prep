a4e861657d4af721249c2b9c890b42d1c430be04
saurabhraj042 <saurabhraj042@gmail.com>
2021-09-20 10:58:16 +0530

    add leetcode soln 73

---
+++ leetcode/arrays/sde sheet/73.set-matrix-zeroes.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool isZeroCol = false;
+        bool isZeroRow = false;
+        
+        for (int i = 0; i < matrix.size(); i++) { //check the first column
+            if (matrix[i][0] == 0) {
+                isZeroCol = true;
+                break;
+            } 
+        }
+        
+        for (int i = 0; i < matrix[0].size(); i++) { //check the first row
+            if (matrix[0][i] == 0) {
+                isZeroRow = true;
+                break;
+            } 
+        }
+        
+        for (int i = 1; i < matrix.size(); i++) { //check except the first row and column
+            for (int j = 1; j < matrix[0].size(); j++) 
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+        }
+        
+        for (int i = 1; i < matrix.size(); i++) { //process except the first row and column
+           for (int j = 1; j < matrix[0].size(); j++) 
+               if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                   matrix[i][j] = 0;
+        }
+        
+        if (isZeroCol) { //handle the first column
+            for (int i = 0; i < matrix.size(); i++) 
+                matrix[i][0] = 0;
+        }
+        
+        if (isZeroRow) { //handle the first row
+            for (int i = 0; i < matrix[0].size(); i++) 
+                matrix[0][i] = 0;
+        }
+        
+    }
+};
