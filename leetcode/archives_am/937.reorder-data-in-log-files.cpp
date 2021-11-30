017f0d1030847789acf2c42ab1b286cad011c9b7
saurabhraj042 <saurabhraj042@gmail.com>
2021-11-30 07:30:15 +0530

    add leetcode soln 937

---
+++ leetcode/archives_am/937.reorder-data-in-log-files.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/reorder-data-in-log-files/
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        vector<string> digitLogs, ans;
+        vector<pair<string, string>> letterLogs;
+        
+        for (string &s : logs) {
+            int i = 0;
+            while (s[i] != ' ') ++i;
+          
+            if (isalpha(s[i + 1])) letterLogs.emplace_back(s.substr(0, i), s.substr(i + 1));
+            else digitLogs.push_back(s);
+        }
+        
+        sort(letterLogs.begin(), letterLogs.end(), [&](auto& a, auto& b) {
+            return a.second == b.second ? a.first < b.first : a.second < b.second;
+        });
+        
+        for (auto &p : letterLogs) ans.push_back(p.first + " " + p.second);
+        for (string &s : digitLogs) ans.push_back(s);
+        return ans;
+    }
+};
